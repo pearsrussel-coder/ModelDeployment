19e80f001b1560fee9da7707d7dcd9886ee5347c
pearsrussel-coder <russelpears1@gmail.com>
2022-08-17 20:37:02 -0500

    changes to config file
    changes to config files

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import pandas as pd
 from starlette.responses import HTMLResponse
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-import tensorflow as tf
 import re
 
 app = FastAPI()
